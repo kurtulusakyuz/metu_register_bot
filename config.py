c9a2f4444efa5b07b3d3835c80d401dfb313c3f9
kurtulusakyuz <kurtulus95@gmail.com>
2026-02-24 11:39:22 +0300

    last commit

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
-USER_AGENT_TEMPLATES = {
-    "chrome": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/{version}.0.0.0 Safari/537.36"
-    ),
-    "firefox": (
-        "Mozilla/5.0 (Windows NT 10.0; rv:{version}.0) "
-        "Gecko/20100101 Firefox/{version}.0"
-    ),
-}
+# USER_AGENT_TEMPLATES = {
+#     "chrome": (
+#         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+#         "AppleWebKit/537.36 (KHTML, like Gecko) "
+#         "Chrome/{version}.0.0.0 Safari/537.36"
+#     ),
+#     "firefox": (
+#         "Mozilla/5.0 (Windows NT 10.0; rv:{version}.0) "
+#         "Gecko/20100101 Firefox/{version}.0"
+#     ),
+# }
 
 IMPERSONATES = ['chrome142','firefox144']
 
 CAPTCHA_PREFETCH = 45
-CAPTCHA_TIMEOUT = 99
+CAPTCHA_TIMEOUT = 130
 TOTAL_ATTEMPTS = 2000
 TWOCAPTCHA_API_KEY = ''
 DEFAULT_SITEKEY = '' #Send me an email.
